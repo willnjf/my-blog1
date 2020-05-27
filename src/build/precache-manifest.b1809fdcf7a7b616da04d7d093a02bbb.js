fb8a47073cb949188dee309dae9c7d156a89fd85
Willy Desktop <willyjiemon@yahoo.fr>
2020-05-27 13:27:19 +0200

    First commit

---
+++ src/build/precache-manifest.b1809fdcf7a7b616da04d7d093a02bbb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05e3a03ea9e01c847bc9777dda4c1355",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6f81eca13ea76cc22da5",
+    "url": "/static/css/main.4a9d094a.chunk.css"
+  },
+  {
+    "revision": "c2be464aa755dd558ed8",
+    "url": "/static/js/2.a38ab3ce.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.a38ab3ce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6f81eca13ea76cc22da5",
+    "url": "/static/js/main.d63af673.chunk.js"
+  },
+  {
+    "revision": "053c8e34507918769709",
+    "url": "/static/js/runtime-main.aecaeffa.js"
+  }
+]);
